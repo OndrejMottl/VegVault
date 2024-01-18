2dcb23b83ed1d53cbea632956e30bddb9e41699d
OndrejMottl <ondrej.mottl@gmail.com>
2024-01-18 11:05:37 +0100

    WIP Add script to import palaeoclimate data (all)

---
+++ R/02_Main_analyses/06_Import_palaeoclimate_data.R
@@ -0,0 +1,401 @@
+#----------------------------------------------------------#
+#
+#
+#                 BIODYNAMICS - VegVault
+#
+#                Import palaeoclimatic data
+#
+#
+#                       O. Mottl
+#                         2023
+#
+#----------------------------------------------------------#
+
+# Download, wrangel, and import palaeoclimate data
+
+#----------------------------------------------------------#
+# 0. Setup -----
+#----------------------------------------------------------#
+
+library(here)
+
+# Load configuration
+source(
+  here::here(
+    "R/00_Config_file.R"
+  )
+)
+
+
+#----------------------------------------------------------#
+# 1. Connect to db -----
+#----------------------------------------------------------#
+
+con <-
+  DBI::dbConnect(
+    RSQLite::SQLite(),
+    paste0(
+      data_storage_path,
+      "Data/VegVault/VegVault.sqlite"
+    )
+  )
+
+DBI::dbListTables(con)
+
+
+#----------------------------------------------------------#
+# 2. Load data -----
+#----------------------------------------------------------#
+
+url_gh_abiotic <-
+  paste0(
+    "https://raw.githubusercontent.com/",
+    "OndrejMottl/BIODYNAMICS-abiotic_data/",
+    "main/"
+  )
+
+data_palaeoclimate_bio1 <-
+  c(
+    "bio01_batch_1_2024-01-02__05b0b43b6640a26c729b0403e711993f__.qs",
+    "bio01_batch_2_2024-01-02__f728c578e64054e96e1671829a1971f2__.qs",
+    "bio01_batch_3_2024-01-02__636dae96f45c34f2f63a579f7bba9ec6__.qs",
+    "bio01_batch_4_2024-01-02__c432f3ecedeae729f35914f02a6f65dc__.qs",
+    "bio01_batch_5_2024-01-02__3d4236481e4131ed665474d6dc7a9b41__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio1"
+  )
+
+data_palaeoclimate_bio4 <-
+  c(
+    "bio04_batch_1_2024-01-02__36c3bf7eaf751dd87c88572616223b9c__.qs",
+    "bio04_batch_2_2024-01-02__74c600653097d990fec92d40beb6a557__.qs",
+    "bio04_batch_3_2024-01-02__92dfb070c075499d2d9b77f901d0f739__.qs",
+    "bio04_batch_4_2024-01-02__5d51cb18bd5c8101eb6e3705fea54ab7__.qs",
+    "bio04_batch_5_2024-01-02__72224b81f4602b6ef6d8229bddb68e55__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio4"
+  )
+
+data_palaeoclimate_bio6 <-
+  c(
+    "bio06_batch_1_2024-01-02__211593e840d69410dbb3fda2e044ca0f__.qs",
+    "bio06_batch_2_2024-01-02__77b394961d94acb5401545fa828a02ca__.qs",
+    "bio06_batch_3_2024-01-02__4817bc0ebf905493c47cdd6032de533c__.qs",
+    "bio06_batch_4_2024-01-02__1d574ea247a0be371242b322dd65fa30__.qs",
+    "bio06_batch_5_2024-01-02__02c03be82d30e91d7cff18612f99ce93__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio6"
+  )
+
+data_palaeoclimate_bio12 <-
+  c(
+    "bio12_batch_1_2024-01-02__879c70c6b0c5734d9158203e0b78234d__.qs",
+    "bio12_batch_2_2024-01-02__3e93995da7101616071866415471080b__.qs",
+    "bio12_batch_3_2024-01-02__66df86e3fe749bd5edf391bb91a14c0d__.qs",
+    "bio12_batch_4_2024-01-02__4ab5a7aab2eff0b639ce9df85210eb96__.qs",
+    "bio12_batch_5_2024-01-02__cb482577798841955c9a77c4c65c4b8c__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio12"
+  )
+
+data_palaeoclimate_bio15 <-
+  c(
+    "bio15_batch_1_2024-01-02__485c339599c32a77b2cb4cd549dff2d7__.qs",
+    "bio15_batch_2_2024-01-02__43c464ceada9e1ed2c94275e6fb1f32b__.qs",
+    "bio15_batch_3_2024-01-02__4a5a703d16ca4c52980bf14353a3bb3f__.qs",
+    "bio15_batch_4_2024-01-02__de127f43a6cf2d51a970c9529d0b61fe__.qs",
+    "bio15_batch_5_2024-01-02__b698ef571dd8f934332e2355d8186bf7__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio15"
+  )
+
+data_palaeoclimate_bio18 <-
+  c(
+    "bio18_batch_1_2024-01-02__2863e2d24f19ff79e4666dc0ca351699__.qs",
+    "bio18_batch_2_2024-01-02__47db5ae8ffa972af3b0196237f6c9282__.qs",
+    "bio18_batch_3_2024-01-02__cc9c13c22f18993480587e7899eb3fb0__.qs",
+    "bio18_batch_4_2024-01-02__7cc2eef1bf68c100a6399cea7aac2f78__.qs",
+    "bio18_batch_5_2024-01-02__82d8f449cbf802b1f40202ba316b111c__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio18"
+  )
+
+data_palaeoclimate_bio19 <-
+  c(
+    "bio19_batch_1_2024-01-02__2ea92fbda0b6218030fbb787c394c2e2__.qs",
+    "bio19_batch_2_2024-01-02__19dc681d8b6b2ab021b1595ed0102cf7__.qs",
+    "bio19_batch_3_2024-01-02__fbe8c00dcc0255265f117eeaa7ceb409__.qs",
+    "bio19_batch_4_2024-01-02__8efb9458536960e5a579b84b1248ba59__.qs",
+    "bio19_batch_5_2024-01-02__e8a5ac69dbeb6dbc06dfcc1c87909d2f__.qs"
+  ) %>%
+  purrr::map(
+    .f = ~ paste0(
+      url_gh_abiotic,
+      "Outputs/Data/Palaoclimate/",
+      .x
+    ) %>%
+      dowload_and_load()
+  ) %>%
+  dplyr::bind_rows() %>%
+  dplyr::mutate(
+    var_name = "bio19"
+  )
+
+data_palaeoclimate_raw <-
+  dplyr::bind_rows(
+    data_palaeoclimate_bio1,
+    data_palaeoclimate_bio4,
+    data_palaeoclimate_bio6,
+    data_palaeoclimate_bio12,
+    data_palaeoclimate_bio15,
+    data_palaeoclimate_bio18,
+    data_palaeoclimate_bio19
+  )
+
+data_palaeoclimate_nested <-
+  data_palaeoclimate_raw %>%
+  tidyr::pivot_wider(
+    names_from = var_name,
+    values_from = value
+  ) %>%
+  tidyr::nest(
+    data_climate = c(
+      time_id, bio1, bio4, bio6, bio12, bio15, bio18, bio19
+    )
+  )
+
+dplyr::glimpse(data_palaeoclimate_nested)
+
+
+#----------------------------------------------------------#
+# 3. Datasets -----
+#----------------------------------------------------------#
+palaeoclimate_dataset_raw <-
+  data_palaeoclimate_nested %>%
+  dplyr::mutate(
+    dataset_type = "abiotic",
+    coord_long = as.numeric(long),
+    coord_lat = as.numeric(lat),
+    # sample_reference = "https://doi.org/10.5194/cp-2021-30",
+    data_source_desc = "gridpoints",
+    dataset_name = paste(
+      "geo", round(coord_long, digits = 2), round(coord_lat, digits = 2),
+      sep = "_"
+    )
+  )
+
+palaeoclimate_dataset_raw_unique <-
+  palaeoclimate_dataset_raw %>%
+  dplyr::distinct(
+    dataset_type, data_source_desc,
+    coord_long, coord_lat,
+    dataset_name
+  ) %>%
+  dplyr::anti_join(
+    dplyr::tbl(con, "Datasets") %>%
+      dplyr::select(dataset_name) %>%
+      dplyr::collect(),
+    by = dplyr::join_by(dataset_name)
+  )
+
+# 3.1 dataset source -----
+
+data_palaeoclimate_data_source_id <-
+  palaeoclimate_dataset_raw_unique %>%
+  dplyr::distinct(data_source_desc) %>%
+  dplyr::anti_join(
+    dplyr::tbl(con, "DatasetSourcesID") %>%
+      dplyr::select(data_source_desc) %>%
+      dplyr::collect(),
+    by = dplyr::join_by(data_source_desc)
+  )
+
+add_to_db(
+  conn = con,
+  data = data_palaeoclimate_data_source_id,
+  table_name = "DatasetSourcesID"
+)
+
+data_palaeoclimate_data_source_id_db <-
+  dplyr::tbl(con, "DatasetSourcesID") %>%
+  dplyr::collect() %>%
+  dplyr::inner_join(
+    palaeoclimate_dataset_raw %>%
+      dplyr::distinct(data_source_desc),
+    by = dplyr::join_by(data_source_desc)
+  )
+
+# 3.2 dataset type -----
+
+data_palaeoclimate_dataset_type_id <-
+  palaeoclimate_dataset_raw %>%
+  dplyr::distinct(dataset_type) %>%
+  dplyr::anti_join(
+    dplyr::tbl(con, "DatasetTypeID") %>%
+      dplyr::select(-dataset_type_id) %>%
+      dplyr::collect(),
+    by = dplyr::join_by(dataset_type)
+  )
+
+add_to_db(
+  conn = con,
+  data = data_palaeoclimate_dataset_type_id,
+  table_name = "DatasetTypeID"
+)
+
+data_palaeoclimate_dataset_type_id_db <-
+  dplyr::tbl(con, "DatasetTypeID") %>%
+  dplyr::collect() %>%
+  dplyr::inner_join(
+    palaeoclimate_dataset_raw %>%
+      dplyr::distinct(dataset_type),
+    by = dplyr::join_by(dataset_type)
+  )
+
+# 3.3 datasets -----
+palaeoclimate_dataset <-
+  palaeoclimate_dataset_raw_unique %>%
+  dplyr::left_join(
+    data_palaeoclimate_data_source_id_db,
+    by = dplyr::join_by(data_source_desc)
+  ) %>%
+  dplyr::left_join(
+    data_palaeoclimate_dataset_type_id_db,
+    by = dplyr::join_by(dataset_type)
+  ) %>%
+  dplyr::select(
+    dataset_name, data_source_id, dataset_type_id,
+    coord_long, coord_lat
+  ) %>%
+  dplyr::anti_join(
+    dplyr::tbl(con, "Datasets") %>%
+      dplyr::select(dataset_name) %>%
+      dplyr::collect(),
+    by = dplyr::join_by(dataset_name)
+  )
+
+add_to_db(
+  conn = con,
+  data = palaeoclimate_dataset,
+  table_name = "Datasets"
+)
+
+palaeoclimate_dataset_id <-
+  dplyr::tbl(con, "Datasets") %>%
+  dplyr::select(dataset_id, dataset_name) %>%
+  dplyr::collect() %>%
+  dplyr::inner_join(
+    palaeoclimate_dataset_raw_unique %>%
+      dplyr::distinct(dataset_name),
+    by = dplyr::join_by(dataset_name)
+  )
+
+
+#----------------------------------------------------------#
+# 4. Samples -----
+#----------------------------------------------------------#
+
+palaeoclimate_samples_raw <-
+  palaeoclimate_dataset_raw %>%
+  dplyr::mutate(
+    data_climate = purrr::map2(
+      .progress = TRUE,
+      .x = data_climate,
+      .y = dataset_name,
+      .f = ~ .x %>%
+        dplyr::mutate(
+          sample_name = paste0(
+            .y, "_", dplyr::row_number()
+          ),
+          age = (-as.numeric(time_id) * 100) + 2000
+        )
+    )
+  ) %>%
+  tidyr::unnest(data_climate)
+
+
+palaeoclimate_samples_raw <-
+  palaeoclimate_dataset_raw %>%
+  tidyr::unnest(data_climate) %>%
+  dplyr::group_by(dataset_name) %>%
+  dplyr::mutate(
+    sample_name = paste0(
+      dataset_name, "_", dplyr::row_number()
+    ),
+    age = (-as.numeric(time_id) * 100) + 2000
+  )
+
+#----------------------------------------------------------#
+# 5. Dataset - Sample -----
+#----------------------------------------------------------#
+
+#----------------------------------------------------------#
+# 5. Climate -----
+#----------------------------------------------------------#
+
+
+#----------------------------------------------------------#
+# 5. Disconnect -----
+#----------------------------------------------------------#
+
+DBI::dbDisconnect(con)
