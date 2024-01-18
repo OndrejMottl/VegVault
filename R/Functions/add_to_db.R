a60a0a16182542bb28e31f8eca39c6cadc6494cd
OndrejMottl <ondrej.mottl@gmail.com>
2024-01-18 14:58:57 +0100

    add_to_db: Add overwrite_test parameter

--- R/Functions/add_to_db.R
+++ R/Functions/add_to_db.R
@@ -4,14 +4,26 @@ add_to_db <- function(
     conn,
     data,
     table_name,
+    overwrite_test = FALSE,
     append = TRUE) {
+  is_valid <- FALSE
+
   if (
-    dplyr::tbl(conn, table_name) %>%
+    isTRUE(overwrite_test)
+  ) {
+    is_valid <- TRUE
+  } else {
+    is_valid <-
+      dplyr::tbl(conn, table_name) %>%
       dplyr::collect() %>%
       dplyr::bind_rows(
         data
       ) %>%
       test_unique_row_in_table()
+  }
+
+  if (
+    isTRUE(is_valid)
   ) {
     message(
       paste("adding", nrow(data), "rows to", table_name, "table")
