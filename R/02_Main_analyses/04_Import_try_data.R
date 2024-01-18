e5236e5422f888bd7e5ee9d209746e465aa0f42f
OndrejMottl <ondrej.mottl@gmail.com>
2024-01-18 14:59:20 +0100

    04_Import_try_data: finish refactor wtih smaller functions

--- R/02_Main_analyses/04_Import_try_data.R
+++ R/02_Main_analyses/04_Import_try_data.R
@@ -163,30 +163,12 @@ try_samples_id <-
 # 5. Dataset - Sample -----
 #----------------------------------------------------------#
 
-data_try_dataset_sample <-
-  try_samples_raw %>%
-  dplyr::distinct(
-    dataset_name, sample_name
-  ) %>%
-  dplyr::left_join(
-    try_dataset_id,
-    by = dplyr::join_by(dataset_name)
-  ) %>%
-  dplyr::left_join(
-    try_samples_id,
-    by = dplyr::join_by(sample_name)
-  ) %>%
-  dplyr::select(
-    dataset_id, sample_id
-  )
-
-dplyr::copy_to(
-  con,
-  data_try_dataset_sample,
-  name = "DatasetSample",
-  append = TRUE
+add_dataset_sample(
+  data_source = try_samples_raw,
+  dataset_id = try_dataset_id,
+  sample_id = try_samples_id,
+  con = con
 )
-
 
 #----------------------------------------------------------#
 # 6. taxa -----
@@ -196,30 +178,10 @@ try_taxa_raw <-
   try_samples_raw %>%
   dplyr::rename(taxon_name = acc_species_name)
 
-try_taxa <-
-  try_taxa_raw %>%
-  dplyr::distinct(taxon_name) %>%
-  dplyr::anti_join(
-    dplyr::tbl(con, "Taxa") %>%
-      dplyr::select(taxon_name) %>%
-      dplyr::collect(),
-    by = dplyr::join_by(taxon_name)
-  )
-
-add_to_db(
-  conn = con,
-  data = try_taxa,
-  table_name = "Taxa"
-)
-
 try_taxa_id <-
-  dplyr::tbl(con, "Taxa") %>%
-  dplyr::select(taxon_id, taxon_name) %>%
-  dplyr::collect() %>%
-  dplyr::inner_join(
-    try_taxa_raw %>%
-      dplyr::distinct(taxon_name),
-    by = dplyr::join_by(taxon_name)
+  add_taxa(
+    data_source = try_taxa_raw,
+    con = con
   )
 
 
@@ -233,107 +195,39 @@ try_traits_raw <-
 
 
 # 7.1 Trait domains -----
-
-try_trait_domain <-
-  try_traits_raw %>%
-  dplyr::distinct(trait_domain_name) %>%
-  dplyr::anti_join(
-    dplyr::tbl(con, "TraitsDomain") %>%
-      dplyr::select(trait_domain_name) %>%
-      dplyr::collect(),
-    by = dplyr::join_by(trait_domain_name)
-  )
-
-add_to_db(
-  conn = con,
-  data = try_trait_domain,
-  table_name = "TraitsDomain"
-)
-
-trait_domain_id <-
-  dplyr::tbl(con, "TraitsDomain") %>%
-  dplyr::select(trait_domain_id, trait_domain_name) %>%
-  dplyr::collect() %>%
-  dplyr::inner_join(
-    try_traits_raw %>%
-      dplyr::distinct(trait_domain_name),
-    by = dplyr::join_by(trait_domain_name)
+try_trait_domain_id <-
+  add_trait_domain(
+    data_source = try_traits_raw,
+    con = con
   )
 
 # 7.2 Traits -----
-
-try_traits <-
-  try_traits_raw %>%
-  dplyr::distinct(trait_domain_name, trait_full_name) %>%
-  dplyr::left_join(
-    trait_domain_id,
-    by = dplyr::join_by(trait_domain_name)
-  ) %>%
-  dplyr::select(-trait_domain_name) %>%
-  dplyr::rename(
-    trait_name = trait_full_name
-  ) %>%
-  dplyr::anti_join(
-    dplyr::tbl(con, "Traits") %>%
-      dplyr::select(trait_name) %>%
-      dplyr::collect(),
-    by = dplyr::join_by(trait_name)
-  )
-
-add_to_db(
-  conn = con,
-  data = try_traits,
-  table_name = "Traits"
-)
-
 try_traits_id <-
-  dplyr::tbl(con, "Traits") %>%
-  dplyr::select(trait_id, trait_name) %>%
-  dplyr::collect() %>%
-  dplyr::inner_join(
-    try_traits_raw %>%
-      dplyr::distinct(trait_full_name),
-    by = dplyr::join_by(trait_name == trait_full_name)
+  add_traits(
+    data_source = try_traits_raw,
+    trait_domain_id = try_trait_domain_id,
+    con = con
   )
+
 
 # 7.3 Trait value -----
 
-try_traits_value <-
-  try_traits_raw %>%
+try_traits_raw %>%
   dplyr::select(
     dataset_name, sample_name,
     trait_name = trait_full_name,
     taxon_name = acc_species_name,
     trait_value
   ) %>%
-  dplyr::left_join(
-    try_dataset_id,
-    by = dplyr::join_by(dataset_name)
-  ) %>%
-  dplyr::left_join(
-    try_samples_id,
-    by = dplyr::join_by(sample_name)
-  ) %>%
-  dplyr::left_join(
-    try_traits_id,
-    by = dplyr::join_by(trait_name)
-  ) %>%
-  dplyr::left_join(
-    try_taxa_id,
-    by = dplyr::join_by(taxon_name)
-  ) %>%
-  dplyr::select(
-    trait_id, dataset_id, sample_id,
-    taxon_id,
-    trait_value
+  add_trait_value(
+    data_source = .,
+    dataset_id = try_dataset_id,
+    samples_id = try_samples_id,
+    traits_id = try_traits_id,
+    taxa_id = try_taxa_id,
+    con = con
   )
 
-dplyr::copy_to(
-  con,
-  try_traits_value,
-  name = "TraitsValue",
-  append = TRUE
-)
 
 #----------------------------------------------------------#
 # 8. Disconect DB -----
