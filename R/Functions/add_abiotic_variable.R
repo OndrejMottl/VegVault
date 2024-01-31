c2f6c56309bbe0f57f5dbdde9acf1915e7c09709
OndrejMottl <ondrej.mottl@gmail.com>
2024-01-31 15:52:20 +0100

    chelsa trace data functions: adding abiotic reference, abiotic variable, etc

---
+++ R/Functions/add_abiotic_variable.R
@@ -0,0 +1,82 @@
+add_abiotic_variable <- function(
+    data_source,
+    con) {
+  assertthat::assert_that(
+    assertthat::has_name(
+      data_source,
+      c(
+        "abiotic_variable_name",
+        "var_unit",
+        "var_reference",
+        "var_detail"
+      )
+    ),
+    msg = "data_source must contain columns: abiotic_variable_name, var_unit, var_reference, var_detail"
+  )
+
+  data_abiotic_variable <-
+    data_source %>%
+    dplyr::select(
+      abiotic_variable_name,
+      var_unit,
+      var_reference,
+      var_detail
+    ) %>%
+    dplyr::distinct(
+      abiotic_variable_name,
+      var_unit,
+      var_reference,
+      var_detail
+    ) %>%
+    dplyr::rename(
+      variable_reference = var_reference,
+    )
+
+  data_reference <-
+    add_abiotic_referecne(
+      data_source = data_abiotic_variable,
+      con = con
+    )
+
+  data_abiotic_variable_with_reference <-
+    data_abiotic_variable %>%
+    dplyr::left_join(
+      data_reference,
+      by = dplyr::join_by(variable_reference == reference_detail)
+    ) %>%
+    dplyr::select(-variable_reference) %>%
+    dplyr::select(
+      abiotic_variable_name = variable_name,
+      abiotic_variable_unit = variable_unit,
+      abiotic_variable_reference = reference_id,
+      measure_details = variable_detail
+    ) %>%
+    dplyr::distinct()
+
+  data_abiotic_variable_with_reference_unique <-
+    data_abiotic_variable_with_reference %>%
+    dplyr::anti_join(
+      dplyr::tbl(con, "AbioticVariable") %>%
+        dplyr::distinct(abiotic_variable_name) %>%
+        dplyr::collect(),
+      by = dplyr::join_by(abiotic_variable_name)
+    )
+
+  add_to_db(
+    conn = con,
+    data = data_abiotic_variable_with_reference_unique,
+    table_name = "AbioticVariable"
+  )
+
+  abiotic_variabe_id <-
+    dplyr::tbl(con, "AbioticVariable") %>%
+    dplyr::select(abiotic_variable_id, abiotic_variable_name) %>%
+    dplyr::collect() %>%
+    dplyr::inner_join(
+      data_source %>%
+        dplyr::distinct(abiotic_variable_name),
+      by = dplyr::join_by(abiotic_variable_name)
+    )
+
+  return(abiotic_variabe_id)
+}
---
+++ R/Functions/add_sample_abiotic_value.R
@@ -0,0 +1,69 @@
+add_sample_abiotic_value <- function(
+    data_source,
+    con,
+    sample_id,
+    abiotic_variable_id) {
+  assertthat::assert_that(
+    assertthat::has_name(
+      data_source,
+      c(
+        "sample_name",
+        "abiotic_variable_name",
+        "value"
+      )
+    ),
+    msg = "data_source must contain columns: sample_name, abiotic_variable_name, value"
+  )
+
+  assertthat::assert_that(
+    assertthat::has_name(
+      sample_id,
+      c(
+        "sample_name",
+        "sample_id"
+      )
+    ),
+    msg = "sample_id must contain columns: sample_name, sample_id"
+  )
+
+  assertthat::assert_that(
+    assertthat::has_name(
+      abiotic_variable_id,
+      c(
+        "abiotic_variable_name",
+        "abiotic_variable_id"
+      )
+    ),
+    msg = "abiotic_variable_id must contain columns: abiotic_variable_name, abiotic_variable_id"
+  )
+
+  data_sample_value <-
+    data_source %>%
+    dplyr::left_join(
+      sample_id,
+      by = dplyr::join_by(sample_name)
+    ) %>%
+    dplyr::left_join(
+      abiotic_variable_id,
+      by = dplyr::join_by(abiotic_variable_name)
+    ) %>%
+    dplyr::select(
+      sample_id, abiotic_variable_id, value
+    )
+
+  data_sample_value_unique <-
+    data_sample_value %>%
+    dplyr::anti_join(
+      dplyr::tbl(con, "AbioticData") %>%
+        dplyr::select(sample_id, abiotic_variable_id) %>%
+        dplyr::collect(),
+      by = dplyr::join_by(sample_id, abiotic_variable_id)
+    )
+
+  add_to_db(
+    conn = con,
+    data = data_sample_value_unique,
+    table_name = "AbioticData",
+    overwrite_test = TRUE
+  )
+}
