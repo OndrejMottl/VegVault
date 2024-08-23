5ad992bac9a6c982e30cfd8205b798e3824e3ad4
Ondřej Mottl <ondrej.mottl@gmail.com>
2024-08-23 10:42:10 +0200

    `11_Check_database.R`: new script to check, analyze and rebuild the databse

---
+++ R/02_Main_analyses/11_Check_database.R
@@ -0,0 +1,108 @@
+#----------------------------------------------------------#
+#
+#
+#                       VegVault
+#
+#                  Check the created DB
+#
+#
+#                       O. Mottl
+#                         2023
+#
+#----------------------------------------------------------#
+
+# Get classification for all taxa in the database
+
+#----------------------------------------------------------#
+# 0. Setup -----
+#----------------------------------------------------------#
+
+library(here)
+
+# Load configuration
+source(
+  here::here(
+    "R/00_Config_file.R"
+  )
+)
+
+verbose <- FALSE
+
+
+#----------------------------------------------------------#
+# 1. Connect to db -----
+#----------------------------------------------------------#
+
+con <-
+  DBI::dbConnect(
+    RSQLite::SQLite(),
+    paste0(
+      data_storage_path,
+      "Data/VegVault/VegVault.sqlite"
+    )
+  )
+
+
+#----------------------------------------------------------#
+# 2. Check connections within DB -----
+#----------------------------------------------------------#
+
+if (
+  isTRUE(verbose)
+) {
+  # check the primary keys
+  DBI::dbGetQuery(
+    con,
+    "
+    SELECT m.name AS table_name, p.name AS column_name
+    FROM sqlite_master m
+    JOIN pragma_table_info(m.name) p
+    WHERE p.pk > 0
+    ORDER BY m.name, p.pk;
+    "
+  ) %>%
+    View()
+
+  # check all indexes
+  DBI::dbGetQuery(
+    con,
+    "
+    SELECT
+      name AS index_name,
+      tbl_name AS table_name,
+      sql
+    FROM
+      sqlite_master
+    WHERE
+      type = 'index'
+    ORDER BY
+      tbl_name, index_name;
+    "
+  ) %>%
+    View()
+}
+
+
+#----------------------------------------------------------#
+# 3. Rebuild and compact the DB -----
+#----------------------------------------------------------#
+
+# WARNING! This will take a while
+
+# Rebuild the database
+DBI::dbExecute(
+  con,
+  "VACUUM;"
+)
+
+# Analyze the database
+DBI::dbExecute(
+  con,
+  "ANALYZE;"
+)
+
+#----------------------------------------------------------#
+# 4. Disconnect -----
+#----------------------------------------------------------#
+
+DBI::dbDisconnect(con)
